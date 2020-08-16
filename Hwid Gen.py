f78c47b5864f8b46051b2f622e27398d21e1dd8e
Cuet <66260320+Cuet1337@users.noreply.github.com>
2020-08-16 16:58:43 +0200

    Add files via upload

---
+++ Hwid Gen.py
@@ -0,0 +1,46 @@
+import random
+
+def hwidgenerator():
+    hwids=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    hwid1 = random.choice((hwids))
+    hwid2 = random.choice((hwids))
+    hwid3 = random.choice((hwids))
+    hwid4 = random.choice((hwids))
+    hwid5 = random.choice((hwids))
+    hwid6 = random.choice((hwids))
+    hwid7 = random.choice((hwids))
+    hwid8 = random.choice((hwids))
+    hwid9 = '-'
+    hwid10 = random.choice((hwids))
+    hwid11 = random.choice((hwids))
+    hwid12 = random.choice((hwids))
+    hwid13 = random.choice((hwids))
+    hwid14 = random.choice((hwids))
+    hwid15 = '-'
+    hwid16 = random.choice((hwids))
+    hwid17 = random.choice((hwids))
+    hwid18 = random.choice((hwids))
+    hwid19 = random.choice((hwids))
+    hwid20 = '-'
+    hwid21 = random.choice((hwids))
+    hwid22 = random.choice((hwids))
+    hwid23 = random.choice((hwids))
+    hwid24 = random.choice((hwids))
+    hwid25 = '-'
+    hwid26 = random.choice((hwids))
+    hwid27 = random.choice((hwids))
+    hwid28 = random.choice((hwids))
+    hwid29 = random.choice((hwids))
+    hwid30 = random.choice((hwids))
+    hwid31 = random.choice((hwids))
+    hwid32 = random.choice((hwids))
+    hwid33 = random.choice((hwids))
+    hwid34 = random.choice((hwids))
+    hwid35 = random.choice((hwids))
+    hwid36 = random.choice((hwids))
+    hwid37 = random.choice((hwids))
+    print(f'{hwid1}{hwid2}{hwid3}{hwid4}{hwid5}{hwid6}{hwid7}{hwid8}{hwid9}{hwid10}{hwid11}{hwid12}{hwid13}{hwid14}{hwid15}{hwid16}{hwid17}{hwid18}{hwid19}{hwid20}{hwid21}{hwid22}{hwid23}{hwid24}{hwid25}{hwid26}{hwid27}{hwid28}{hwid29}{hwid30}{hwid31}{hwid32}{hwid33}{hwid34}{hwid35}{hwid36}{hwid37}')
+
+while True:
+    hwidgenerator()
